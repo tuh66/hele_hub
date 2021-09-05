81854a87ea9bb795b25783352fa0dcdfdab5c941
tuh66 <89264108+tuh66@users.noreply.github.com>
2021-09-05 19:46:44 +0200

    Update a.js

--- scripts/js/a.js
+++ scripts/js/a.js
@@ -1,6 +1,6 @@
-document.write('<script src="https://raw.githubusercontent.com/tuh66/hele_hub/main/scripts/js/b.highlight.min.js" type="text/javascript"></script>');
-document.write('<link media="all" type="text/css" rel="stylesheet" href="https://raw.githubusercontent.com/tuh66/hele_hub/main/scripts/css/default.min.css">');
-document.write('<link media="all" type="text/css" rel="stylesheet" href="https://raw.githubusercontent.com/tuh66/hele_hub/main/scripts/css/tomorrow.min.css">');
+document.write('<script src="https://tuh66.github.io/hele_hub/scripts/js/b.highlight.min.js" type="text/javascript"></script>');
+document.write('<link media="all" type="text/css" rel="stylesheet" href="https://tuh66.github.io/hele_hub/scripts/css/default.min.css">');
+document.write('<link media="all" type="text/css" rel="stylesheet" href="https://tuh66.github.io/hele_hub/scripts/css/tomorrow.min.css">');
 document.write('<style>#pastee-t6qAqJJCCyIASdyl-content { margin-top: 0px !important; background-color: #f7f7f9; border: 1px solid #e1e1e8; white-space: pre-wrap; } #pastee-t6qAqJJCCyIASdyl-content > code { margin-top: 0px !important; }</style>');
 document.write('<pre id="pastee-t6qAqJJCCyIASdyl-content"><code>loadstring(game:HttpGet((\&#039;https://raw.githubusercontent.com/tuh66/hele_hub/main/scripts/lua/source.lua\&#039;), true))()\n</code></pre>');
 function initEmbeddedPaste_t6qAqJJCCyIASdyl() { hljs.highlightBlock(document.getElementById('pastee-t6qAqJJCCyIASdyl-content')); }
