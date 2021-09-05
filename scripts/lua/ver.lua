c6d93bcd74fd084f527970a53a28e4bd5139e4c3
tuh66 <89264108+tuh66@users.noreply.github.com>
2021-09-05 20:41:49 +0200

    Update ver.lua

--- scripts/lua/ver.lua
+++ scripts/lua/ver.lua
@@ -1,6 +1,6 @@
-if _G.ver == '1.0.0' then else
+if _G.ver == '1.0.1' then else
   game.StarterGui:SetCore('SendNotification', {
     Title = 'HeleHub';
-    Text = "Your Version of HeleHub is old!\nGet A new wersion bro"
+    Text = "Your Version of HeleHub is old or cracked!\nGet A Real Version bro"
   })
 end
