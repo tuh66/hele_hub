e1bde2cdd9479ee3c28b7dc33fa55143ef5a1905
tuh66 <89264108+tuh66@users.noreply.github.com>
2021-09-05 18:28:19 +0200

    Create scr.js

---
+++ scr.js
@@ -0,0 +1,8 @@
+document.write('<script src="https://cdn.jsdelivr.net/highlight.js/9.0.0/highlight.min.js" type="text/javascript"></script>');
+document.write('<link media="all" type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/highlight.js/9.0.0/styles/default.min.css">');
+document.write('<link media="all" type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/highlight.js/9.0.0/styles/tomorrow.min.css">');
+document.write('<style>#pastee-t6qAqJJCCyIASdyl-content { margin-top: 0px !important; background-color: #f7f7f9; border: 1px solid #e1e1e8; white-space: pre-wrap; } #pastee-t6qAqJJCCyIASdyl-content > code { margin-top: 0px !important; }</style>');
+document.write('<pre id="pastee-t6qAqJJCCyIASdyl-content"><code>loadstring(game:HttpGet((\&#039;https://tuh66.github.io/hele_hub/lua/source.lua\&#039;), true))()\n</code></pre>');
+function initEmbeddedPaste_t6qAqJJCCyIASdyl() { hljs.highlightBlock(document.getElementById('pastee-t6qAqJJCCyIASdyl-content')); }
+addEventListener('DOMContentLoaded', initEmbeddedPaste_t6qAqJJCCyIASdyl, false);
+addEventListener('load', initEmbeddedPaste_t6qAqJJCCyIASdyl, false);
